9a6931247cbe5697da24096a64a54b878a47e48b
Sakshi-Bharavada <bharavadasakshi@gmail.com>
2025-06-03 14:52:36 +0530

    Added files of portfolio
    Added essential files of portfolio

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    const targetId = this.getAttribute('href');
+    if (targetId.startsWith('#')) {
+      e.preventDefault();
+      document.querySelector(targetId).scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+}); 
